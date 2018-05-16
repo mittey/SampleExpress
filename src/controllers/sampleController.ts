7e1e6ca605ef5ba1f1a67db675875214bc75a4f9
Dmitry <d.k.kulikov@gmail.com>
2018-05-16 16:19:35 +0300

    Some fixes

---
+++ src/controllers/sampleController.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export default class SampleController {
+    constructor() {
+
+    }
+
+    public hello(req: Request, res: Response, next: NextFunction): void {
+        res.json({ response: "hello world" });
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Application } from "express";
+import SampleController from "./controllers/sampleController";
 // import MusicController from "./controllers/MusicController"
 
 export default class Routes {
 
     // _musicController: MusicController = new MusicController();
+    _sampleController: SampleController = new SampleController();
 
     constructor(app: Application) {
         // app.route("/api/music/play").post(this._musicController.play);
         // app.route("/api/music/stop").post(this._musicController.stop);
+        app.route("/api/sample").get(this._sampleController.hello);
     }
 }
